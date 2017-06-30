b5e6e303178ba1699397e0c421d4a44ad6c4145a
Naookie Sato <nsato@cs.uoregon.edu>
2017-06-29 19:19:10 -0700

    fixed return to main page button: note it is unsecure

--- naookiesato-com/port_listener.js
+++ naookiesato-com/port_listener.js
@@ -14,19 +14,19 @@ function pageNotFound(request, response) {
 }
 
 function onRequest(request, response, next) {
-    if (request.method == "GET" && request.url == '/') {
+    if (request.url == '/' || request.url == '/index.html') {
         console.log("about to serve page: www.naookiesato.com/");
         response.writeHead(200, {"Content-Type": "text/html"});
         fs.createReadStream("./index.html").pipe(response);
     }
-    else if (request.method == "POST" && request.url == '/schedule_page.html') {
+    else if (request.url == '/schedule_page.html') {
         process.stdout.write("about to serve page: www.naookiesato.com");
         process.stdout.write(request.url);
         process.stdout.write('\n');
         response.writeHead(200, {"Content-Type": "text/html"});
         fs.createReadStream("./schedule_page.html").pipe(response);
     }
-    else if (request.method == "GET" && request.url == '/IMG_6145.PNG') {
+    else if (request.url == '/IMG_6145.PNG') {
         process.stdout.write("about to serve image: www.naookiesato.com");
         process.stdout.write(request.url);
         process.stdout.write('\n');
@@ -36,6 +36,8 @@ function onRequest(request, response, next) {
     else {
         process.stdout.write("Cannot serve ")
         process.stdout.write(request.url);
+        process.stdout.write(" with ");
+        process.stdout.write(request.method);
         process.stdout.write('\n');
         pageNotFound(request, response);
     }
@@ -43,8 +45,12 @@ function onRequest(request, response, next) {
 }
 
 function logRequest(request, response, next) {
-        process.stdout.write("request recieved: ");
+        process.stdout.write(new Date().toString());
+        process.stdout.write(": request recieved: ");
         process.stdout.write(request.url);
+        //This doesnt work
+        //process.stdout.write(" from client ");
+        //process.stdout.write(request.connection.remoteAddress.toString());
         process.stdout.write('\n');
         next();
 }
