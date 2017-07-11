33d8a9690c652605c6bfa14167fae2e0fd90faa1
Naookie Sato <nsato@cs.uoregon.edu>
2017-07-11 11:19:38 -0700

    forgot to commit

--- secret-necrosato-com/js/module2.js
+++ secret-necrosato-com/js/module2.js
@@ -1,2 +1,13 @@
 console.log("module2");
+
+var buttons = [];
+for (var i = 0; i < 9; i++) {
+    buttons.push([i, false]);
+}
+function toggleButton(buttonIndex) {
+    buttons[buttonIndex][1] = !buttons[buttonIndex][1];
+    nextRand = (buttonIndex * 7) % 9;
+    buttons[nextRand][1] = !buttons[nextRand][1];
+}
+
 alert("You've found a secret site!");
